c55831356f1f93dfd38b36a4dc81928dd49a0947
FarazRanjbari <franjbar@our.ecu.edu.au>
2026-04-22 13:57:12 +0800

    Clean up commented code in streamlit_app.py
    Removed commented-out code for dataframe display and API call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,16 +16,12 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients'
     , my_dataframe
     , max_selections = 5
 )
-
-#st.write(ingredients_list)
-#st.text(ingredients_list)
 
 if ingredients_list:
     ingredients_strings = ''
@@ -39,19 +35,12 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_strings + """','"""+name_on_order + """')"""
     
-    #st.write(my_insert_stmt)
-    #st.stop()
     
     time_to_insert = st.button('Submit Order')
 
-    #st.write(my_insert_stmt)
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
     
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), user_container_width=True)
