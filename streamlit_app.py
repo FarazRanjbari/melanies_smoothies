e073781d59dec37e68ea91a26248c60378897b81
FarazRanjbari <franjbar@our.ecu.edu.au>
2026-04-22 14:27:16 +0800

    Fetch fruit options from Snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,8 @@ st.write("The Name on the Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
 pd_df = my_dataframe.to_pandas()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
